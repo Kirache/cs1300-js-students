42f7bc305dc531f157a62211e5cfc52cca6075cf
Kirache <49444055+Kirache@users.noreply.github.com>
2020-10-28 16:43:02 -0700

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=U4f9NEGGtYZ2u44OJMVPbp-RI3Oa-c6ZWUz16kekuwM";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -25,7 +25,40 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      //console.log(request)
+      handleResponse(request.response);
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+const handleResponse = (requestResponse) => {
+    const jsonified = JSON.parse(requestResponse);
+    const plantsArray = jsonified.data;
+    const grasses1753 = plantsArray.filter((arrayItem) => {
+        return (arrayItem.year == 1753 & arrayItem.family == "Poaceae");
+    });
+    displayInfo(grasses1753);
+}
+
+const imageSource = (url) => { url == null ? console.log("Is null") : console.log("Is not null") };
+
+const displayInfo = (info) => {
+    const plantDiv = document.createElement("div");
+    plantDiv.style.display = "none";
+
+    for (i = 0;  i<info.length; i++){
+        const header = document.createElement("h2");
+        header.innerText = info[i].common_name;
+        plantDiv.appendChild(header);
+
+        const image = document.createElement("img");
+        const source = info[i].image_url == null ? "https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg" : info[i].image_url;
+        image.src = source;
+        plantDiv.appendChild(image);
+
+        document.getElementById("plants").appendChild(plantDiv);
+    }
+
+    document.getElementById("loading").style.display = "none";
+    plantDiv.style.display = "block";
+}
